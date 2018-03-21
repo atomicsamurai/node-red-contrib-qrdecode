1e950af9f9c96114afd7fae0f579f2a418d16324
Sandeep Chaturvedi <schaturvedi@gmail.com>
2018-03-21 11:10:48 -0600

    now scanning from buffer

--- qrdecode.js
+++ qrdecode.js
@@ -8,26 +8,32 @@ module.exports = function (RED) {
         RED.nodes.createNode(this, config);
         var node = this;
         node.on('input', function (msg) {
-            node.log(`decoding QR data from ${msg.payload}`);
-
+            //console.log(`decoding QR data from ${msg.payload}`);
+            console.log(`here1`);
+            var buf = msg.payload;
             //var buffer = fs.readFileSync(__dirname + '/test.jpg');
-            var buffer = fs.readFileSync(msg.payload);
-            Jimp.read(buffer, function (err, image) {
-                if (err) {
-                    node.error(`error when reading image: ${err}`, msg);
-                    // TODO handle error
-                }
-                var qr = new QrCode();
-                qr.callback = function (err, value) {
+            //var buffer = fs.readFileSync(msg.payload);
+            if (buf instanceof Buffer) {
+                Jimp.read(buf, function (err, image) {
                     if (err) {
-                        node.error(`error when decoding: ${err}`, msg);
+                        node.error(`error when reading image: ${err}`, {});
+                        // TODO handle error
+                    } else {
+                        var qr = new QrCode();
+                        qr.callback = function (err, value) {
+                            if (err) {
+                                node.error(`error when decoding: ${err}`, {});
+                                return;
+                            } else {
+                                console.log(value.result);
+                                msg.payload = value.result;
+                                node.send(msg);
+                            }
+                        }
+                        qr.decode(image.bitmap);
                     }
-                    node.log(value.result);
-                    msg.payload = value.result;
-                    node.send(msg);
-                };
-                qr.decode(image.bitmap);
-            });
+                });
+            }
         });
     }
     RED.nodes.registerType("qrdecode", QRDecodeNode);
