1388b99d67196cc064a7cc71a4055162dda60306
Sandeep Chaturvedi <schaturvedi@gmail.com>
2018-03-21 11:34:27 -0600

    more error handling

--- qrdecode.js
+++ qrdecode.js
@@ -9,30 +9,37 @@ module.exports = function (RED) {
         var node = this;
         node.on('input', function (msg) {
             //console.log(`decoding QR data from ${msg.payload}`);
-            console.log(`here1`);
             var buf = msg.payload;
             //var buffer = fs.readFileSync(__dirname + '/test.jpg');
             //var buffer = fs.readFileSync(msg.payload);
             if (buf instanceof Buffer) {
                 Jimp.read(buf, function (err, image) {
                     if (err) {
-                        node.error(`error when reading image: ${err}`, {});
+                        node.warn(`error when reading image: ${err}`, {});
+                        msg.payload = {status : 1, message: `error when reading image: ${err}`};
+                        node.send(msg);
                         // TODO handle error
                     } else {
                         var qr = new QrCode();
                         qr.callback = function (err, value) {
                             if (err) {
-                                node.error(`error when decoding: ${err}`, {});
-                                return;
+                                node.warn(`error when decoding: ${err}`, {});
+                                msg.payload = {status : 1, message: `error when decoding image: ${err}`};
+                                node.send(msg);
                             } else {
                                 console.log(value.result);
-                                msg.payload = value.result;
+                                msg.payload = {status : 0, message: `success`, value: `${value.result}`};
+                                //msg.payload = value.result;
                                 node.send(msg);
                             }
                         }
                         qr.decode(image.bitmap);
                     }
                 });
+            } else {
+                node.warn("invalid input", {});
+                msg.payload = {status : 1, message: `invalid input`};
+                node.send(msg);
             }
         });
     }
