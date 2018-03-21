7cfe9fd9ef8f9e0f0420e7ab0708d26cfc299353
Sandeep Chaturvedi <schaturvedi@gmail.com>
2018-03-21 09:05:39 -0600

    logging and error handling

--- qrdecode.js
+++ qrdecode.js
@@ -8,27 +8,23 @@ module.exports = function (RED) {
         RED.nodes.createNode(this, config);
         var node = this;
         node.on('input', function (msg) {
-            node.log(`data = ${msg}`);
             node.log(`decoding QR data from ${msg.payload}`);
 
             //var buffer = fs.readFileSync(__dirname + '/test.jpg');
             var buffer = fs.readFileSync(msg.payload);
             Jimp.read(buffer, function (err, image) {
                 if (err) {
-                    node.error(`error when reading image: ${err}`);
+                    node.error(`error when reading image: ${err}`, msg);
                     // TODO handle error
                 }
                 var qr = new QrCode();
                 qr.callback = function (err, value) {
                     if (err) {
-                        node.error(`error when decoding: ${err}`);
-                        return;
+                        node.error(`error when decoding: ${err}`, msg);
                     }
                     node.log(value.result);
                     msg.payload = value.result;
-                    //msg.payload = msg.payload.toLowerCase();
                     node.send(msg);
-                    //console.log(value);
                 };
                 qr.decode(image.bitmap);
             });
